85928cb08a25855d45f2bf9937b77022e641ad66
Катерина Осадчая <monzolevskaya@gmail.com>
2021-10-10 13:59:12 +0300

    promises tasks

---
+++ js/task-2.js
@@ -0,0 +1,36 @@
+const users = [
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false },
+];
+
+//===================НЕ ИИСПОЛЬЗУЕТ callback-функцию,возвращает Promise===============//
+const toggleUserState = (allUsers, userName) => {
+  return new Promise((resolve) => {
+    const updatedUsers = allUsers.map((user) =>
+      user.name === userName ? { ...user, active: !user.active } : user
+    );
+    resolve(updatedUsers);
+  });
+};
+
+const logger = (updatedUsers) => console.table(updatedUsers);
+
+toggleUserState(users, "Mango").then(logger);
+toggleUserState(users, "Lux").then(logger);
+
+
+//===================ИИСПОЛЬЗУЕТ callback-функцию===============//
+// const toggleUserState = (allUsers, userName, callback) => {
+//   const updatedUsers = allUsers.map((user) =>
+//     user.name === userName ? { ...user, active: !user.active } : user
+//   );
+
+//   callback(updatedUsers);
+// };
+
+// const logger = (updatedUsers) => console.table(updatedUsers);
+
+// toggleUserState(users, "Mango", logger);
+// toggleUserState(users, "Lux", logger);
